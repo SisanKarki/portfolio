46b21b1e19862d2a96cc18c4d4363402f9fc6ad0
SisanKarki <sisankarki786@gmail.com>
2024-12-19 22:50:07 +0545

    css change

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
-﻿<?php
+﻿ <?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'sisankarki786@gmail.com';
 
 
 if($_POST) {
@@ -66,4 +66,4 @@ if($_POST) {
 
 }
 
-?>
+?> 
